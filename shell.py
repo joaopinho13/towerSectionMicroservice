e374010c777b5c8ec91abe94b4185d4e80a37f05
joaopinho <joaopinho99@outlook.com>
2023-06-17 16:14:42 +0100

    Shell class created

---
+++ shell.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 17 10:39:55 2023
+
+@author: joao
+"""
+
+class Shell:
+    def __init__(self, unique_id, section_position, height, top_diameter,
+                 bot_diameter, thickness, steel_density):
+        self.unique_id = unique_id
+        self.section_position = section_position
+        self.height = height
+        self.top_diameter = top_diameter
+        self.bot_diameter = bot_diameter
+        self.thickness = thickness
+        self.steel_density = steel_density
+    
+    def __str__(self):
+        return'Shell number (ID): {}, Position: {}, Height: {}, Top diameter: {},'\
+               ' Bottom diameter: {}, Thickness: {}, Steel density: {}'.format(
+               self.unique_id, self.section_position, self.height,
+               self.top_diameter, self.bot_diameter, self.thickness,
+               self.steel_density)
